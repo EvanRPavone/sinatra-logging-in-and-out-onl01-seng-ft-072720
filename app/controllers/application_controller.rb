98fc025dc2c082f32943d72fcd3714d1557d6095
Evan Pavone <evanrpavone@gmail.com>
2020-09-09 17:27:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
       erb :account
     else
       erb :error
